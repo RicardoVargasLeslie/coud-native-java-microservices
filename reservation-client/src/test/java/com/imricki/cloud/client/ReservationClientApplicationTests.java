7d0da405f3b2fbbfcbb8976a283f067626158206
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-11-28 18:11:54 +0000

    created client

---
+++ reservation-client/src/test/java/com/imricki/cloud/client/ReservationClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imricki.cloud.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
