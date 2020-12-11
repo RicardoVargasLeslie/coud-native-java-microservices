ec1d6f26b7d18cfcfd19178cf735ce31aa68f5ca
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-12-11 15:27:42 +0000

    aded reservation client and reservation service

---
+++ reservation-service/src/main/java/com/imricki/cloud/reservation/ReservationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.imricki.cloud.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationServiceApplication.class, args);
+	}
+
+}
