97f4871825da295002f57a07946e8e5fdeb642ae
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-12-12 16:58:35 +0000

    aded rest resource and controller methods

--- reservation-service/src/main/java/com/imricki/cloud/reservation/repo/ReservationRepository.java
+++ reservation-service/src/main/java/com/imricki/cloud/reservation/repo/ReservationRepository.java
@@ -5,11 +5,13 @@ import java.util.Collection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.imricki.cloud.reservation.entity.Reservation;
 
 @RepositoryRestResource
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
+	@RestResource(path = "by-name")
 	Collection<Reservation> findByReservationName(@Param("rn") String rn);
 }
--- reservation-service/src/main/java/com/imricki/cloud/reservation/rest/MessageRestController.java
+++ reservation-service/src/main/java/com/imricki/cloud/reservation/rest/MessageRestController.java
@@ -1,8 +1,23 @@
 package com.imricki.cloud.reservation.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MessageRestController {
 
+	private final String value;
+
+	@RequestMapping(method = RequestMethod.GET, value = "/message")
+	public String read() {
+		return this.value;
+	}
+
+	@Autowired
+	public MessageRestController(@Value("${message}") String value) {
+		this.value = value;
+	}
 }
